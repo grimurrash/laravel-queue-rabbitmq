8c8e77bb68b7b778c3aaae4a69699811196d2834
rashit.sabirov <rashit.sabirov1999@gmail.com>
2022-12-13 11:50:23 +0300

    php 8.2

--- src/Consumer.php
+++ src/Consumer.php
@@ -197,12 +197,12 @@ class Consumer extends Worker
      * @param  int  $status
      * @return int
      */
-    public function stop($status = 0): int
+    public function stop($status = 0, $options = null): int
     {
         // Tell the server you are going to stop consuming.
         // It will finish up the last message and not send you any more.
         $this->channel->basic_cancel($this->consumerTag, false, true);
 
-        return parent::stop($status);
+        return parent::stop($status, $options);
     }
 }
